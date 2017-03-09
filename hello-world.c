32b5ebd88854e8c07343203050ca8f2a8a2e8b58
ObrienHuang <470215834@qq.com>
2017-03-09 08:52:16 +0800

    Update hello-world.c

--- hello-world.c
+++ hello-world.c
@@ -1,9 +1,7 @@
-```C
-  #include <stdio.h>
+#include <stdio.h>
   
-  int main(int argc , char * argv)
-  {
-    
+int main(int argc , char * argv)
+{
+    printf("hello world!\n");
     return 0;
-  }
-  ```
+}
